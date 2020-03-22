f026035e2ec03d6857cff36ec5815081d06781dd
irynaomelchenko <37276001+irynaomelchenko@users.noreply.github.com>
2020-03-22 19:32:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const getUsersBtn = document.querySelector('.get-users-btn');
 const usersList = document.querySelector('.users-list');
 const userInfo = document.querySelector('.user-info-outer');
-const userInfoCard = document.querySelector('.user-info-card')
+const userInfoCard = document.querySelector('.user-info-card');
+const form = document.forms['form-add-new-user'];
+const inputName = document.querySelector('#input-name');
+const inputUserName = document.querySelector('#input-user-name');
+const inputEmail = document.querySelector('#input-email');
+const inputAdd = document.querySelector('#input-add');
+let formData = new FormData(document.forms.formAddNewUser);
+const objOfUsers = {}
 
 function getUsers(cb) {
   const xhr = new XMLHttpRequest();
@@ -21,12 +28,8 @@ function getUsers(cb) {
 function liTemplate(user) {
   const li = document.createElement('li');
   li.classList.add('list-item');
-  li.setAttribute('data-user-id', user.id)
-  const a = document.createElement('a');
-  a.classList.add('user-link')  
-  a.setAttribute('href', '#')
-  a.textContent = user.name;
-  li.appendChild(a)
+  li.setAttribute('data-user-id', user.id);
+  li.textContent = user.name;
   return li;
 }
 
@@ -36,34 +39,28 @@ function renderUsers(response) {
   response.forEach(user => {
     const li = liTemplate(user);
     fragment.appendChild(li);
+    objOfUsers[user.id] = user;
   })
 
-  usersList.appendChild(fragment);
+  if (usersList.hasChildNodes()) {
+    usersList.innerHTML = ""
+    usersList.appendChild(fragment)
+  } else usersList.appendChild(fragment);
 }
 
 getUsersBtn.addEventListener('click', e => {
-  getUsers(renderUsers)
+  getUsers(renderUsers)  
 })
 
 usersList.addEventListener('click', onLiHandler);
 
 function getUserInfo(cb, id) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', `https://jsonplaceholder.typicode.com/users/${id}`);
-  xhr.addEventListener('load', () => {
-    const response = JSON.parse(xhr.responseText);
-    cb(response);
-  });
-
-  xhr.addEventListener('error', () =>{
-    console.log('error');
-  });
-
-  xhr.send();
+  let user = objOfUsers[id]
+  cb(user)
 }
 
 function cardTemplate(user) {
-  userInfoCard.innerHTML = `Name: ${user.name}<br>Username: ${user.username}<br>Email: ${user.email}<br>Address: ${user.address.street} str., ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}<br>Phone: ${user.phone}<br>Web-site: ${user.website}<br>Company: ${user.company.name}`
+  userInfoCard.innerHTML = `<p>Name: ${user.name}</p><p>Username: ${user.username}</p><p>Email: ${user.email}</p>`
   return userInfoCard;
 }
 
@@ -76,10 +73,36 @@ function renderUserInfo(user) {
 }
 
 function onLiHandler({ target }) {
-  if (target.classList.contains('user-link')) {
-    const parent = target.closest('[data-user-id]');
-    const id = parent.dataset.userId;
+  if (target.classList.contains('list-item')) {
+    const id = target.dataset.userId;
     getUserInfo(renderUserInfo, id)
   }
 }
 
+function renderNewUser(user) {
+  const fragment = document.createDocumentFragment();
+  const li = liTemplate(user);
+  fragment.appendChild(li);
+  usersList.appendChild(fragment);
+}
+
+inputAdd.addEventListener('click', e => {
+  fetch('https://jsonplaceholder.typicode.com/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      id: +usersList.lastChild.dataset.userId+1,
+      name: inputName.value,
+      username: inputUserName.value,
+      email: inputEmail.value
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+  .then(json => {
+    objOfUsers[json.id] = json;
+    renderNewUser(json);
+    form.reset();
+  })
+})
